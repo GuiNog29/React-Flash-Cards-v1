eb19544962dae965cb1edec674d192851758d1ac
Guilherme Almeida <guimathiasgui@gmail.com>
2021-08-26 20:47:26 -0300

    Adicionando FlashCard e estilizando o mesmo

---
+++ src/pages/FlashCardsPage.jsx
@@ -0,0 +1,13 @@
+import FlashCard from '../components/FlashCard';
+import Header from '../components/Header';
+import Main from '../components/Main';
+
+export default function FlashCardsPage() {
+  return (
+    <>
+      <Header>React-Flash-Cards-v1</Header>
+
+      <Main><FlashCard/></Main>
+    </>
+  );
+}
